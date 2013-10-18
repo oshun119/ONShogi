ad4f2885280f432eac776eca3359b32196ec1ea7
nisshy <mn.kyoto@gmail.com>
2013-10-18 20:35:51 +0900

    Administrator#run, Administrator#toStringを実装．

--- onshogi/Administrator.java
+++ onshogi/Administrator.java
@@ -61,11 +61,51 @@ public class Administrator extends Object implements Runnable {
 	 */
 	@Override
 	public void run() {
-		// TODO 自動生成されたメソッド・スタブ
+		System.out.println(this);
+		
+		while(!currentState.isEnd()) {
+			Move nextMove = null;
+			
+
+			if(this.isBlackTurn)
+				nextMove = this.blackPlayer.nextMove(this.currentState.clone());
+			else
+				nextMove = this.whitePlayer.nextMove(this.currentState.clone());
+			
+			if(!this.currentState.isLegalMove(nextMove)) {
+				System.out.println("不正な手が入力されました．");
+				break;
+			}
+			
+			this.currentState.makeAMove(nextMove);
+			this.isBlackTurn = !this.isBlackTurn;
+			System.out.println(this);
+		}
+		
+		String winner;
+		if(this.isBlackTurn)
+			winner = this.whitePlayer.toString();
+		else
+			winner = this.blackPlayer.toString();
+		
+		System.out.println(winner + "win!");
 	}
 	
 	@Override
 	public String toString() {
-		return "";
+		StringBuilder builder = new StringBuilder();
+		
+		builder.append("black:");
+		builder.append(this.blackPlayer);
+		builder.append(" white: ");
+		builder.append(this.whitePlayer);
+		
+		if(isBlackTurn)
+			builder.append("\nIt is black turn now.\n");
+		else
+			builder.append("\nIt is white turn now.\n");
+		
+		builder.append(this.currentState);
+		return builder.toString();
 	}
 }
