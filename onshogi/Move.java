58991a8c2cceaf0f1b5b90f82b5effb18c4d2dcd
nisshy <mn.kyoto@gmail.com>
2013-10-19 00:54:47 +0900

    Moveコンストラクタの修正．

--- onshogi/Move.java
+++ onshogi/Move.java
@@ -5,6 +5,7 @@ import java.awt.Point;
 /**
  * 移動する駒, 移動元, 移動先, 成るか の情報を渡すためのクラス
  * @author oshun119
+ * @author nisshy
  */
 public class Move extends Object{
 	/**
@@ -27,10 +28,30 @@ public class Move extends Object{
 	 * 駒の情報,今のところPiece
 	 */
 	private Piece piece;
-	
-	
-	public Move(Point fromPoint, Point toPoint, boolean hasPromoted) {
 		
+	/**
+	 * 盤上の駒を動かす場合．
+	 * @param fromPoint 駒の移動元の座標． 
+	 * @param toPoint 駒の移動先の座標．
+	 * @param hasPromoted 駒が成るか否か．
+	 */
+	public Move(Point fromPoint, Point toPoint, boolean hasPromoted) {
+		this.fromPoint = fromPoint;
+		this.toPoint = toPoint;
+		this.hasPromoted = hasPromoted;
+		this.piece = null;
+	}
+	
+	/**
+	 * 駒を打つ場合．
+	 * @param toPoint 駒を打つ位置
+	 * @param piece 打つ駒．
+	 */
+	public Move(Point toPoint, Piece piece) {
+		this.fromPoint = null;
+		this.toPoint = toPoint;
+		this.hasPromoted = false;
+		this.piece = piece;
 	}
 	
 	/**
